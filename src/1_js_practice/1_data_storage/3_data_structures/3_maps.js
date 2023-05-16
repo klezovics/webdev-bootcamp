30d1ce3d637f34f21787b0e8acd0bba181abe822
klezovich <arthur@trevor.io>
2023-05-16 19:54:42 +0200

    Fix

--- src/1_js_practice/1_data_storage/3_data_structures/3_maps.js
+++ src/1_js_practice/1_data_storage/3_data_structures/3_maps.js
@@ -1,13 +1,1 @@
 
-function createAndPrintVariableViaEachPossibleKeyword() {
-    // Explain the difference between them in comments
-}
-
-function showHowToUseTheSetClass() {
-}
-
-function showHowToUseTheMapClass() {
-}
-
-function showHowToUseADate() {
-}
