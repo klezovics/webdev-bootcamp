41ddbb94e584faa6a024759718324f80ded62a54
iulianasimin <iulianasimin@gmx.net>
2023-05-16 19:20:12 +0200

    add object exercise

--- src/1_js_practice/1_data_storage/1_types/3_complex_types_objects.js
+++ src/1_js_practice/1_data_storage/1_types/3_complex_types_objects.js
@@ -1,3 +1,15 @@
 function createTwoObjectsWith3DifferentFieldsEach() {
+    const o1 = {
+        name: "Artur",
+        age: 25
+    }
+
+    const o2 = {
+        name: "Iuliana",
+        age: 35
+    }
+
+    console.log(o1, o2);
 
 }
+createTwoObjectsWith3DifferentFieldsEach();
