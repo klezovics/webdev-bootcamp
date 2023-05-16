e320be7c16b0efa85d3a5797ffb5d32e44d9e3f5
iulianasimin <iulianasimin@gmx.net>
2023-05-16 19:24:22 +0200

    add function exercise

--- src/1_js_practice/1_data_storage/1_types/4_complex_types_functions.js
+++ src/1_js_practice/1_data_storage/1_types/4_complex_types_functions.js
@@ -1,3 +1,10 @@
 function saveFunctionToAVariableAndUseVariableToCallFunction() {
-
+  const x = function () {
+    console.log("Hello");
+  };
+  x();
 }
+
+saveFunctionToAVariableAndUseVariableToCallFunction();
+
+
