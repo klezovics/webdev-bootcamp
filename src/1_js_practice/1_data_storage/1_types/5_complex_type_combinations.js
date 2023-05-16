ab39caf4963811fdae931a30f7e9ab0375833a50
iulianasimin <iulianasimin@gmx.net>
2023-05-16 19:33:18 +0200

    add object array exercise

--- src/1_js_practice/1_data_storage/1_types/5_complex_type_combinations.js
+++ src/1_js_practice/1_data_storage/1_types/5_complex_type_combinations.js
@@ -1,8 +1,34 @@
 function createAndPrintAnArrayOfArrays() {
+  const arr = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+  ];
+
+  console.log(arr);
 }
 
 function createAndPrintArrayOfObjects() {
+  const arr = [
+    { a: "Hello", b: "World" },
+
+    {
+      a: "Hello",
+      b: "Test",
+    },
+  ];
+  console.log(arr);
 }
 
 function createAndPrintObjectSomeOfFieldsOfWhichAreArrays() {
+
+    const o1 = {
+        a: [
+            "Hello", 12
+        ],
+        b: [
+            1, 2, 3
+        ]
+    }
+    console.log(o1);
 }
+createAndPrintObjectSomeOfFieldsOfWhichAreArrays();
