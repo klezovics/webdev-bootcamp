19b1a02039aba0fa39583fe3eb66545b2185a27b
iulianasimin <iulianasimin@gmx.net>
2023-05-16 19:57:16 +0200

    typeof exercise

--- src/1_js_practice/1_data_storage/2_casting/3_typeof_operator.js
+++ src/1_js_practice/1_data_storage/2_casting/3_typeof_operator.js
@@ -1,3 +1,10 @@
 function createVariableOfEveryTypeAndApplyTypeOfOperator() {
-
+  const srt = "wer";
+  const num = 2;
+  const b = true;
+  const n = null;
+  let a;
+  console.log(typeof srt, typeof num,typeof b,typeof n,typeof a);
 }
+
+createVariableOfEveryTypeAndApplyTypeOfOperator();
