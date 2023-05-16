a8212c40cc8951059f601a74454bd93526306de2
klezovich <arthur@trevor.io>
2023-05-16 21:13:44 +0200

    Add example class

--- src/1_js_practice/3_classes/classes.js
+++ src/1_js_practice/3_classes/classes.js
@@ -1,2 +1,22 @@
+// Here's an example of a class in JavaScript.
+class Car {
+    constructor(name) {
+        this.name = name;
+    }
+
+    drive() {
+        console.log(`Driving the ${this.name}`);
+        // Additional logic for driving the car goes here
+    }
+}
+
+// Example usage:
+const myCar = new Car("Tesla");
+myCar.drive(); // Output: Driving the Tesla
+
+const otherCar = new Car("Toyota");
+otherCar.drive(); // Output: Driving the Toyota
+
+
 //Task 1: Create a simple counter class.
 //The class should have the following methods: increment, decrement, get, and reset.
