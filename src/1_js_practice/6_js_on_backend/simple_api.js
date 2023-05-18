fb913b93feb44cabbe51d54d55424806c0ddbc8f
klezovich <arthur@trevor.io>
2023-05-18 17:52:37 +0200

    Add code example of a simple api

---
+++ src/1_js_practice/6_js_on_backend/simple_api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Endpoint for handling GET requests
+app.get('/api/greeting', (req, res) => {
+    const name = req.query.name || 'Anonymous';
+    const greeting = `Hello, ${name}!`;
+
+    res.json({ greeting });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`API server is running on port ${port}`);
+});
