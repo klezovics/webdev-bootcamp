0734852734589312b95259b1418e7adc4adf1e38
klezovich <arthur@trevor.io>
2023-05-18 15:10:35 +0200

    Add exercises on the topic of functions

---
+++ src/1_js_practice/2_data_processing/5_functions/README.md
@@ -0,0 +1,56 @@
+Certainly! Functions are a fundamental concept in JavaScript and are essential for organizing and reusing code. They allow you to define a block of code that can be executed multiple times with different inputs and produce a desired output. Let's start with the basics of functions in JavaScript:
+
+**1. Function Declaration:**
+A function in JavaScript is declared using the `function` keyword, followed by the function name, a list of parameters (inputs), and a code block (function body) enclosed in curly braces. Here's an example of a simple function declaration:
+
+```javascript
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+```
+
+In the above example, `greet` is the function name, and `name` is a parameter. The function body consists of a single statement that logs a greeting to the console, using the provided `name` parameter.
+
+**2. Function Invocation (Calling):**
+Once a function is defined, it can be invoked (called) to execute the code inside the function body. To call a function, use the function name followed by parentheses. If the function has parameters, provide the corresponding values (arguments) inside the parentheses. Here's how we would call the `greet` function:
+
+```javascript
+greet("John");
+```
+
+The above code will invoke the `greet` function with the argument `"John"`, resulting in the output: `"Hello, John!"`.
+
+**3. Function Return:**
+Functions can optionally return a value using the `return` statement. The returned value can be used by the caller. Here's an example:
+
+```javascript
+function add(a, b) {
+  return a + b;
+}
+
+let result = add(2, 3);
+console.log(result); // Output: 5
+```
+
+In the above code, the `add` function takes two parameters `a` and `b` and returns their sum using the `return` statement. The returned value is then assigned to the variable `result` and logged to the console.
+
+**4. Function Parameters and Arguments:**
+Parameters are variables declared in the function signature, which act as placeholders for values that will be passed when the function is called. Arguments are the actual values passed into a function during the function call. In the previous examples, `name`, `a`, and `b` are parameters, and `"John"`, `2`, and `3` are arguments.
+
+**5. Function Scope:**
+Variables declared inside a function are scoped to that function, meaning they are only accessible within the function body. This concept is known as "function scope." Conversely, variables declared outside of any function have "global scope" and can be accessed from anywhere in the code.
+
+**6. Function Expressions:**
+In addition to function declarations, JavaScript also supports function expressions. A function expression is an anonymous function assigned to a variable. Here's an example:
+
+```javascript
+let square = function(num) {
+  return num * num;
+};
+
+console.log(square(5)); // Output: 25
+```
+
+In the above code, an anonymous function is assigned to the variable `square`. The function takes a `num` parameter and returns its square.
+
+These are the fundamental concepts of functions in JavaScript. Functions play a crucial role in modularizing code, promoting code reuse, and enabling more organized and maintainable JavaScript applications. As a junior developer, understanding functions and their usage is essential for your programming journey.
--- src/1_js_practice/2_data_processing/5_functions/functions.js
+++ src/1_js_practice/2_data_processing/5_functions/functions.js
@@ -1,47 +1,43 @@
-// Following the example of printHello() function, write the code for the following functions.
+/*
+1. Write a function called `calculateArea` that takes the length and width of a rectangle as parameters and returns its area.
 
-function helloPrinter() {
-    console.log("Hello World!");
-}
+2. Write a function called `isPrime` that takes a number as a parameter and returns `true` if it's a prime number, and `false` otherwise.
 
-function printHello() {
-    helloPrinter()
-}
+3. Write a function called `capitalize` that takes a string as a parameter and returns the same string with the first letter capitalized.
 
-function getCurrentYear() {}
+4. Write a function called `reverseArray` that takes an array as a parameter and returns a new array with the elements reversed.
 
-function generateRandomNumber() {}
+5. Write a function called `countVowels` that takes a string as a parameter and returns the number of vowels in the string.
 
-function isWeekend() {}
+6. Write a function called `findMaxValue` that takes an array of numbers as a parameter and returns the maximum value in the array.
 
-function getBrowserName() {}
+7. Write a function called `isPalindrome` that takes a string as a parameter and returns `true` if it's a palindrome, and `false` otherwise.
 
-function isLeapYear() {}
+8. Write a function called `calculateFactorial` that takes a number as a parameter and returns its factorial.
 
-function getRandomColor() {}
+9. Write a function called `sumArray` that takes an array of numbers as a parameter and returns the sum of all the numbers.
 
-function getSquareRoot() {}
+10. Write a function called `truncateString` that takes a string and a number as parameters and returns a truncated version of the string with the specified number of characters.
 
-function isPalindrome() {}
+11. Write a function called `isEven` that takes a number as a parameter and returns `true` if it's even, and `false` otherwise.
 
-function getGreeting() {}
+12. Write a function called `removeDuplicates` that takes an array as a parameter and returns a new array with the duplicate elements removed.
 
-function isPrimeNumber() {}
+13. Write a function called `titleCase` that takes a string as a parameter and returns the same string with each word capitalized.
 
-function getFirstDayOfWeek() {}
+14. Write a function called `findLongestWord` that takes an array of strings as a parameter and returns the longest word in the array.
 
-function getFileSize() {}
+15. Write a function called `calculateBMI` that takes a person's weight (in kilograms) and height (in meters) as parameters and returns their BMI (Body Mass Index).
 
-function capitalizeString() {}
+16. Write a function called `endsWith` that takes two strings as parameters and returns `true` if the first string ends with the second string, and `false` otherwise.
 
-function isEvenOrOdd() {}
+17. Write a function called `isLeapYear` that takes a year as a parameter and returns `true` if it's a leap year, and `false` otherwise.
 
-function getSecondsSinceEpoch() {}
+18. Write a function called `findAverage` that takes an array of numbers as a parameter and returns the average of all the numbers.
 
-function getMaxNumber() {}
+19. Write a function called `sumDigits` that takes a number as a parameter and returns the sum of its digits.
 
-function reverseString() {}
+20. Write a function called `getUniqueValues` that takes an array as a parameter and returns a new array with only the unique values from the original array.
 
-function getFactorial() {}
-
-function convertToUpperCase() {}
+These exercises cover a variety of concepts and challenges related to JavaScript functions. They will help you practice writing functions, working with different data types, and manipulating arrays and strings. Happy coding!
+ */
