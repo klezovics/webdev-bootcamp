1d0ad9f83d7c2e7451b60ce5951077405da01416
iulianasimin <iulianasimin@gmx.net>
2023-05-18 15:31:09 +0200

    added Set object exercise

---
+++ src/1_js_practice/1_data_storage/3_data_structures/4_sets.js
@@ -0,0 +1,11 @@
+const o1 = new Set();
+
+o1.add('sun');
+o1.add('23');
+// console.log(o1);
+// console.log(o1.has('sun'));
+// o1.delete('sun');
+// console.log(o1);
+// console.log(o1.size);
+
+o1.forEach((el) => console.log(el+ "Hello") );
