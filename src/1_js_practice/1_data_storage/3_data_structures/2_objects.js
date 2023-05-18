d1a5a746c8553d63b802d978bde5c9d018e6dfa8
iulianasimin <iulianasimin@gmx.net>
2023-05-18 15:08:00 +0200

    add complited exercise

---
+++ src/1_js_practice/1_data_storage/3_data_structures/2_objects.js
@@ -0,0 +1,16 @@
+const o1 = {
+    a: "s",
+    b: 2,
+    c: [1,2,3]
+}
+
+// console.log(o1.a);
+// console.log(o1["b"]);
+// console.log(Object.keys(o1));
+// console.log(Object.values(o1));
+// console.log(Object.entries(o1));
+
+// const o2 = Object.assign({}, o1);
+// console.log(o2);
+
+console.log(o1.hasOwnProperty('b'));
