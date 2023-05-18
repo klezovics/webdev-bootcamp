1db90dc2c33d363cec37e701da8a8b74265df591
iulianasimin <iulianasimin@gmx.net>
2023-05-18 15:20:56 +0200

    objects methods exercises

--- src/1_js_practice/1_data_storage/3_data_structures/3_maps.js
+++ src/1_js_practice/1_data_storage/3_data_structures/3_maps.js
@@ -1,1 +1,14 @@
+const o1 = new Map();
+
+o1.set('name', 'Iuliana');
+o1.set('age', 32);
+// console.log(o1.get('name'));
+// console.log(o1.has("age"));
+// o1.delete('age');
+// console.log(o1);
+// o1.clear();
+// console.log(o1);
+// console.log(o1.size);
+
+o1.forEach((el) => console.log(el+2) );
 
