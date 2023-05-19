7f31de2d2550cfa8278f1907bff4c93cd3fda8b0
klezovich <arthur@trevor.io>
2023-05-19 14:26:34 +0200

    Add deps and new demo component

--- src/3_css_practice/3_styling/1_typography/TypographyDemo.js
+++ src/3_css_practice/3_styling/1_typography/TypographyDemo.js
@@ -1,6 +1,18 @@
+
+const RedP = styled.div`
+    color: red;
+`
+
+const SourceSansPro = styled.div`
+  font-family: 'Source Sans Pro', sans-serif;
+`
+
+
 const TypographyDemo = () => (
     <>
         <div>Typography demo</div>
+        <RedP>This text is red</RedP>
+        <SourceSansPro>This text is in Source Sans Pro</SourceSansPro>
     </>
 )
 
