bc294296a9e11f13b80b63b92a573be7c0f1bf4c
iulianasimin <iulianasimin@gmx.net>
2023-05-19 19:58:39 +0200

    add execrice

--- src/1_js_practice/2_data_processing/2_loops/loops.js
+++ src/1_js_practice/2_data_processing/2_loops/loops.js
@@ -1,15 +1,37 @@
+const ar1 = [1,2,3,4,5,8,7,5,9,5];
 
-function createABasicForLoop() {
+// function createABasicForLoop() {
+// for (i=35; i > 0;){
+//     i= i-5
+//     console.log(i);
+// }
 
-}
+// }
+// 
 
 function createABasicWhileLoop() {
+    let i = 5;
+    while (i<=25) {
+        console.log(i);
+        i++;
+        
+    }
 
 }
+// createABasicWhileLoop();
 
 function createABasicDoWhileLoop() {
+    let i = 5;
+    do {
+        
+        console.log(i);
+        i++;
+    }
+    while (i<20);
+
 
 }
+createABasicDoWhileLoop();
 
 
 function printFirstTenNumbers() {
