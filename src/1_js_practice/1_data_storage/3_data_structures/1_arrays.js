1d8dcd646dec001c612dad0df9b8634d280698ac
iulianasimin <iulianasimin@gmx.net>
2023-05-19 19:17:22 +0200

    added Set object exercisead

--- src/1_js_practice/1_data_storage/3_data_structures/1_arrays.js
+++ src/1_js_practice/1_data_storage/3_data_structures/1_arrays.js
@@ -1,32 +1,48 @@
 const array =[1, 2, 3];
-array.push("q");
-console.log(array);
+// array.push("q");
+// console.log(array);
 
-array.pop();
-console.log(array);
+// array.pop();
+// console.log(array);
 
-array.shift();
-console.log(array);
+// array.shift();
+// console.log(array);
 
-array.unshift(4, "a");
-console.log(array);
+// array.unshift(4, "a");
+// console.log(array);
 
-const array2 = ["a2", "d1"];
-array3 = array.concat(array2);
-console.log(array3);
+// const array2 = ["a2", "d1"];
+// array3 = array.concat(array2);
+// console.log(array3);
 
-const array4 = array.slice(1);
-console.log(array4);
+// const array4 = array.slice(1);
+// console.log(array4);
 
-array.forEach((element) =>console.log(element));
+// array.forEach((element) =>console.log(element));
 
-const array6 = array.map((el) =>el+2 );
-console.log(array6);
-
-const array7 = array.filter((el) => el>3);
+// const array6 = array.map((el) =>el+2 );
+// console.log(array6);
+const array1=[1,2,3,4,5,6,7,8,9,10];
+function numberToEnglishName(n) {
+    const numberToEnglishName = new Map();
+    numberToEnglishName.set(1,"one");
+    numberToEnglishName.set(2,"two");
+    numberToEnglishName.set(3,"three");
+    numberToEnglishName.set(4,"four");
+    numberToEnglishName.set(5,"five");
+    numberToEnglishName.set(6,"six");
+    numberToEnglishName.set(7,"seven");
+    numberToEnglishName.set(8,"eight");
+    numberToEnglishName.set(9,"nine");
+    numberToEnglishName.set(10,"ten");
+    return numberToEnglishName.get(n);
+}
+const myFunction = (el) => (numberToEnglishName(el).includes("n"));
+const array7 = array1.filter(myFunction);
 console.log(array7);
-const array8 = array.find((el) =>(el=="a"));
-console.log(array8);
+// console.log(array7);
+// const array8 = array.find((el) =>(el=="a"));
+// console.log(array8);
 
 
 
---
+++ src/1_js_practice/2_data_processing/1_equality_operations/equality_comparison.js
@@ -0,0 +1,6 @@
+const num = 2;
+const srt = "2";
+console.log(num==srt); //output: true, because implicite type convercion 
+console.log(num===srt); //output: false, because strict equality 
+
+
