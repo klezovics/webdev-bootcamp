e794a3c41b9273fd9541c3447bfab87a4e7088b7
klezovich <arthur@trevor.io>
2023-05-21 01:45:14 +0200

    add examples of easy loops

---
+++ src/1_js_practice/2_data_processing/2_loops/easy_loops.js
@@ -0,0 +1,25 @@
+const array = [1, 3, 4, 5, 6, 2];
+
+// 1. Create a loop using for-of
+// Purpose: iterate through elements of an array
+// for (let arrayElement of array) {
+//   console.log("next array element:",arrayElement);
+// }
+
+// 2. Create a loop using forEach array method
+// Purpose: iterate through elements of an array
+// array.forEach(arrayElement => {
+//   console.log('next array element:', arrayElement);
+// });
+
+const exchange_rate = {
+    USD: 1.2,
+    EUR: 4.3,
+    GBP: 2.11,
+};
+
+// 3. Create a loop using for-in
+// Purpose: Iterate through object keys
+// for(let key in exchange_rate) {
+//    console.log(key,":",exchange_rate[key])
+// }
