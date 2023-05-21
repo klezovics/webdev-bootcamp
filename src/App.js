446b6d8e682e2e0db32cc34a0f8af5d866f15b54
klezovich <arthur@trevor.io>
2023-05-21 13:28:10 +0200

    Move files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import MaterialDemo from "./3_css_practice/6_libraries/2_material/MaterialDemo";
 import TypographyDemo from "./3_css_practice/3_styling/1_typography/TypographyDemo";
-import StarWarsFilm from "./2_react_practice/4_consume_rest_api/StarWarsFilm";
+import StarWarsFilm from "./2_react_practice/4_consume_rest_api/1_react_query/StarWarsFilm";
 import {QueryClient} from "react-query";
-import StarWarsFilmWrapper from "./2_react_practice/4_consume_rest_api/StarWarsFileWrapper";
+import StarWarsFilmWrapper from "./2_react_practice/4_consume_rest_api/1_react_query/StarWarsFileWrapper";
 
 function App() {
     const queryClient = new QueryClient();
