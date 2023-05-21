a7c9ddbc040485884c4c9448a2a0e06a553e0dd8
klezovich <arthur@trevor.io>
2023-05-21 13:08:47 +0200

    Add code which makes juliana eat fruits

---
+++ src/1_js_practice/2_data_processing/2_loops/lectures/juliana_loops.js
@@ -0,0 +1,53 @@
+const fruits = ['banana','orange','kiwi']
+
+// Let's teach Juliana how to eat fruits !!!
+
+// Eating fruits using forEach loop
+// fruits.forEach((fruit) => {
+//     console.log('Juliana cuts ' + fruit)
+//     console.log('Juliana eats ' + fruit)
+// });
+
+// Eating fruits using for-of loop
+// for(let currentFruit of fruits) {
+//     console.log('Juliana cuts ' + fruit)
+//     console.log('Juliana eats ' + currentFruit)
+// }
+
+// Eating fruits using for loop
+// for(let arrayIndex=0; arrayIndex < fruits.length; arrayIndex++) {
+//     const fruit = fruits[arrayIndex]
+//     console.log('Juliana cuts ' + fruit)
+//     console.log('Juliana eats ' + fruit)
+// }
+
+// Eating fruits using while loop
+// let arrayIndex = 0
+// while(arrayIndex < fruits.length) {
+//     const fruit = fruits[arrayIndex]
+//     console.log('Juliana cuts ' + fruit)
+//     console.log('Juliana eats ' + fruit)
+//     arrayIndex++
+// }
+
+// Eating fruits using do-while loop
+// let arrayIndex = 0
+// do {
+//     const fruit = fruits[arrayIndex]
+//     console.log('Juliana cuts ' + fruit)
+//     console.log('Juliana eats ' + fruit)
+//     arrayIndex++
+// } while(arrayIndex < fruits.length)
+
+
+// Displaying fruit preferences using for-in loop
+// const fruitPreferences = {
+//     banana: 'nice',
+//     orange: 'ok',
+//     kiwi: 'AWESOME!'
+// }
+//
+// for(let fruit in fruitPreferences) {
+//     console.log('Juliana thinks ' + fruit + ' is ' + fruitPreferences[fruit])
+// }
+
