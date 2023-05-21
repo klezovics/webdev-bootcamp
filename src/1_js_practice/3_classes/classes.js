55be90b624bca73c5df7151c3499dd51c7bcf1d0
klezovich <arthur@trevor.io>
2023-05-21 20:38:54 +0200

    Add class examples

--- src/1_js_practice/3_classes/classes.js
+++ src/1_js_practice/3_classes/classes.js
@@ -1,5 +1,8 @@
 // Here's an example of a class in JavaScript.
 // Create the car blueprint
+
+// Class = Name + Constructor + Fields + Methods
+
 class Car {
 
     //manufacturing process!
@@ -21,6 +24,23 @@ myCar.drive(); // Output: Driving the Tesla
 const otherCar = new Car("Toyota");
 otherCar.drive(); // Output: Driving the Toyota
 
+class NPrinter {
+    constructor(n) {
+        this.n = n
+    }
+
+    print(str) {
+        let result = ""
+        for(let ii=1; ii<=this.n; ii++) {
+            result=result + str;
+        }
+
+        console.log("Result:",result);
+    }
+}
+
+const n = new NPrinter(4)
+n.print("acb")
 
 //Task 1: Create a simple counter class.
 //The class should have the following methods: increment, decrement, get, and reset.
