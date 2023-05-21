7d00a7c0cab7d9c6b7d4bb4886fcae8ad38442ef
iulianasimin <iulianasimin@gmx.net>
2023-05-21 15:52:13 +0200

    add loop exer

--- src/1_js_practice/2_data_processing/2_loops/lectures/lecture_1/take_care_of_kids.js
+++ src/1_js_practice/2_data_processing/2_loops/lectures/lecture_1/take_care_of_kids.js
@@ -4,4 +4,32 @@ function takeCareOf(kidName) {
     console.log('Juliana puts ' + kidName + ' to sleep')
 }
 
-const kids = ['Pavel', 'Julia', 'Ivan']
+const kids = ['Pavel', 'Julia', 'Ivan'];
+
+// for (const kidName of kids) {
+
+//     takeCareOf(kidName);
+    
+// }
+
+// kids.forEach((kidName) => {
+//     takeCareOf(kidName);
+// })
+
+// for (let arrayIndex = 0; arrayIndex < kids.length; arrayIndex++) {
+//     const kidName = kids[arrayIndex];
+//     takeCareOf(kidName);    
+// }
+// let i = 0;
+// while (i<kids.length) {
+//     const kidName = kids[i];
+//     takeCareOf(kidName);
+//     i++;
+    
+// }
+let i = 0;
+do {
+    const kidName = kids[i];
+        takeCareOf(kidName);
+        i++;
+} while (i<kids.length);
