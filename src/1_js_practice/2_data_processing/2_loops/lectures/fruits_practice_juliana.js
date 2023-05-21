7a9f4c7a7983d57c0e813a43d06bf4972857b3e4
iulianasimin <iulianasimin@gmx.net>
2023-05-21 15:30:58 +0200

    add loop exercises

---
+++ src/1_js_practice/2_data_processing/2_loops/lectures/fruits_practice_juliana.js
@@ -0,0 +1,38 @@
+const vegetables = ["patato", "carrot", "eggplant"];
+
+// vegetables.forEach((vegetable) => {
+//   console.log("Juliana cuts " + vegetable);
+//   console.log("Juliana eats " + vegetable);
+// });
+
+// for (const vegetable of vegetables) {
+//     console.log("Juliana cuts " + vegetable);
+//     console.log("Juliana eats " + vegetable);
+// }
+
+// for (let i = 0; i < vegetables.length; i++) {
+//     const vegetable = vegetables[i];
+//     console.log("Juliana cuts " + vegetable);
+//     console.log("Juliana eats " + vegetable);
+    
+// }
+// let i = 0;
+// while (i<vegetables.length) {
+//     const vegetable = vegetables[i];
+//     console.log("Juliana cuts " + vegetable);
+//     console.log("Juliana eats " + vegetable);
+
+//     i++;
+    
+// }
+
+let i = 0;
+
+do {
+    const vegetable = vegetables[i];
+    console.log("Juliana cuts " + vegetable);
+    console.log("Juliana eats " + vegetable);
+
+    i++;
+    
+} while (i<vegetables.length);
