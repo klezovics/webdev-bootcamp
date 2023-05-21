08d70566b53ffed3b675b4dc3b2b47c16e6d51c5
klezovich <arthur@trevor.io>
2023-05-21 15:40:09 +0200

    Add exercise

---
+++ src/1_js_practice/2_data_processing/2_loops/lectures/lecture_1/take_care_of_kids.js
@@ -0,0 +1,5 @@
+function takeCareOf(kidName) {
+    console.log('Juliana feeds ' + kidName)
+    console.log('Juliana plays with ' + kidName)
+    console.log('Juliana puts ' + kidName + ' to sleep')
+}
