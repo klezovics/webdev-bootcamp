a0be2b683e6db0716ea4628d0adfeacfb95035b1
klezovich <arthur@trevor.io>
2023-05-21 13:26:53 +0200

    Add REST api consumption via ReactQuery

---
+++ src/2_react_practice/4_consume_rest_api/StarWarsFileWrapper.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import StarWarsFilm from './StarWarsFilm';
+
+const StarWarsFilmWrapper = () => {
+    const queryClient = new QueryClient();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <StarWarsFilm />
+        </QueryClientProvider>
+    );
+};
+
+export default StarWarsFilmWrapper;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import MaterialDemo from "./3_css_practice/6_libraries/2_material/MaterialDemo";
 import TypographyDemo from "./3_css_practice/3_styling/1_typography/TypographyDemo";
+import StarWarsFilm from "./2_react_practice/4_consume_rest_api/StarWarsFilm";
+import {QueryClient} from "react-query";
+import StarWarsFilmWrapper from "./2_react_practice/4_consume_rest_api/StarWarsFileWrapper";
 
 function App() {
+    const queryClient = new QueryClient();
+
     return (
         <div className="App">
-            <TypographyDemo/>
+           <StarWarsFilmWrapper/>
         </div>
     );
 }
