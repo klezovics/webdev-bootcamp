39c1f27e18678cb71214cb4eefd6d44c04ef9ddd
iulianasimin <iulianasimin@gmx.net>
2023-05-21 15:41:20 +0200

    add loop exercises

--- src/1_js_practice/2_data_processing/2_loops/lectures/lecture_1/pavel_drives_cars.js
+++ src/1_js_practice/2_data_processing/2_loops/lectures/lecture_1/pavel_drives_cars.js
@@ -1,3 +1,28 @@
-const cars = ['BMW','Volvo','Mercedes','Ferrari']
+const cars = ["BMW", "Volvo", "Mercedes", "Ferrari"];
 
 // Must print "Pavel drives <Car name> car" for every car in the array
+
+cars.forEach((car)=>{
+    console.log("Pawel drives " + car);
+
+})
+
+// for (const car of cars) {
+//     console.log("Pawel drives " + car);
+// }
+
+// for (let i = 0; i < cars.length; i++) {
+//     console.log("Pawel drives " + cars[i]);
+
+// }
+// let i = 0;
+// while (i<cars.length) {
+//     console.log("Pawel drives " + cars[i]);
+//     i++;
+
+// }
+// let i = 0;
+// do {
+//   console.log("Pawel drives " + cars[i]);
+//   i++;
+// } while (i<cars.length);
