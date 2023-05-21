02f40184e36ceba50df57b74330c0ee696d3fa63
iulianasimin <iulianasimin@gmx.net>
2023-05-21 16:33:35 +0200

    addn exercise

---
+++ src/1_js_practice/2_data_processing/3_if_else/if_else2.js
@@ -0,0 +1,10 @@
+let a = 18;
+if(a<16){
+  console.log("NO alcohol");
+} else if(a>=16 && a<18){
+console.log("Beer");
+} else if(a>=18 && a<21) {
+  console.log("Beer and Wine");
+} else{
+  console.log("all")
+}
