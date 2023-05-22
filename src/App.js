58e066fdf85b9dc24165edde910ae1baf0be0c31
klezovich <arthur@trevor.io>
2023-05-22 20:53:32 +0200

    Add simple implementation of tabs component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import {QueryClient} from "react-query";
 import StarWarsFilmViaAxios from "./2_react_practice/4_consume_rest_api/3_axios/StarWarsFilmViaAxios";
 import ChangeColor from "./2_react_practice/1_component_basics/ChangeColor";
 import Carousel from "./2_react_practice/1_component_basics/Carousel";
+import Tabs from "./2_react_practice/1_component_basics/Tabs";
 
 function App() {
     const queryClient = new QueryClient();
 
     return (
         <div className="App">
-           <Carousel/>
+           <Tabs/>
         </div>
     );
 }
