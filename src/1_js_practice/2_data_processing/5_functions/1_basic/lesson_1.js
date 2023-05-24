1294c229b8cc20fef321ed19b670ff259869dd09
iulianasimin <iulianasimin@gmx.net>
2023-05-24 18:39:40 +0200

    add code

--- src/1_js_practice/2_data_processing/5_functions/1_basic/lesson_1.js
+++ src/1_js_practice/2_data_processing/5_functions/1_basic/lesson_1.js
@@ -1,16 +1,21 @@
-const expect = require('expect.js');
+const expect = require("expect.js");
 
 // Input: Number
 // Output: Sum of number digits
 // Example: 123 -> 1 + 2 + 3 = 6
 function sumOfDigits(num) {
-    return 0
+  // convert a number to string and string to array
+  let numberArray = num.toString().split("").map(Number);
+  // use for loop
+  let sum = 0;
+  numberArray.forEach((num) => sum +=num );
+  return sum;
 }
 
 expect(sumOfDigits(0)).to.equal(0);
 expect(sumOfDigits(1)).to.equal(1);
 expect(sumOfDigits(2)).to.equal(2);
-expect(sumOfDigits(10)).to.equal(10);
+expect(sumOfDigits(10)).to.equal(1);
 expect(sumOfDigits(55)).to.equal(10);
 expect(sumOfDigits(1000)).to.equal(1);
 expect(sumOfDigits(9999)).to.equal(36);
