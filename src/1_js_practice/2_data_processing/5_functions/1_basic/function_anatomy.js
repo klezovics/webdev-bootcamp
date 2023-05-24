74848bbdff12b51a502408792955630da37aad14
klezovich <arthur@trevor.io>
2023-05-24 18:19:17 +0200

    Add test examples

---
+++ src/1_js_practice/2_data_processing/5_functions/1_basic/function_anatomy.js
@@ -0,0 +1,27 @@
+// Function exist to extract reusable code and remove code duplication
+// Function can be declared or used !
+// Functions have 2 types: ordinary and arrow functions -> different creation syntax, same usage syntax
+
+// Functions has: input and output
+// Function converts inputs to outputs
+// Inputs -> functional arguments
+// Outputs -> return statement
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+console.log("isEven(2):",isEven(2));
+console.log("isEven(17):",isEven(17));
+
+// You can also have functions without any arguments
+function sayHello() {
+    console.log("Hello!");
+}
+
+function getMagicNumber() {
+    return 42;
+}
+
+// Function attributes: name, arguments, body, return value
+
+
---
+++ src/1_js_practice/2_data_processing/5_functions/1_basic/testing_functions.js
@@ -0,0 +1,12 @@
+const expect = require('expect.js');
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+
+expect(add(1,2)).to.equal(3);
+console.log('First test passed!')
+
+expect(add(10,20)).to.equal(30);
+console.log('Second test passed!')
