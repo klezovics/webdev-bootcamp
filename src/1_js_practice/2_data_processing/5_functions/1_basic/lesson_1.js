50e8af1f2216d4f5e3425c6b7d007c0f1cb76886
iulianasimin <iulianasimin@gmx.net>
2023-05-24 19:06:56 +0200

    add code

--- src/1_js_practice/2_data_processing/5_functions/1_basic/lesson_1.js
+++ src/1_js_practice/2_data_processing/5_functions/1_basic/lesson_1.js
@@ -32,7 +32,6 @@ const expect = require("expect.js");
 // expect(sumOfNumbersInArray([1, 2, 3, 4])).to.equal(10);
 // expect(sumOfNumbersInArray([55, -55])).to.equal(0);
 
-
 // Function must always return 42 irregarless of the input
 // function always42(x,y) {
 //     return 42;
@@ -42,7 +41,6 @@ const expect = require("expect.js");
 // expect(always42(99,-1)).to.equal(42);
 // expect(always42(-200,-2)).to.equal(42);
 // expect(always42(-1, 9)).to.equal(42);
-
 
 // Input: Array of strings
 // Output: String with all the strings from the array joined together
@@ -57,16 +55,22 @@ const expect = require("expect.js");
 // expect(joinStringsArray(["a"])).to.equal("a");
 // expect(joinStringsArray(["ab","bc","cd"])).to.equal("abbccd");
 
-
 function calculateAverage(numbers) {
+  if (numbers.length === 0) return 0
+  
+  let sum = 0;
+  numbers.forEach((num) => (sum += num));
+  let result = sum / numbers.length;
+
+  return result;
 }
 
 expect(calculateAverage([])).to.equal(0);
 expect(calculateAverage([1])).to.equal(1);
-expect(calculateAverage([2,2])).to.equal(2);
-expect(calculateAverage([1,2])).to.equal(1.5);
-expect(calculateAverage([1,2,3])).to.equal(2);
-expect(calculateAverage([1,2,3,4])).to.equal(2.5);
-expect(calculateAverage([-100,100])).to.equal(0);
+expect(calculateAverage([2, 2])).to.equal(2);
+expect(calculateAverage([1, 2])).to.equal(1.5);
+expect(calculateAverage([1, 2, 3])).to.equal(2);
+expect(calculateAverage([1, 2, 3, 4])).to.equal(2.5);
+expect(calculateAverage([-100, 100])).to.equal(0);
 
 console.log("All tests passed!!!");
