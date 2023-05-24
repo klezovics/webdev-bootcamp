d7793ae6d7e0c11c161b6c40ab624e415676433a
klezovich <arthur@trevor.io>
2023-05-25 00:29:14 +0200

    Improve test infrastucture

---
+++ src/1_js_practice/2_data_processing/5_functions/4_tasks/kata_1.test.js
@@ -0,0 +1,9 @@
+test('test can correctly sum numbers in an array', () => {
+
+    const sumOfNumbersInArray = (numbers) => {
+        return 0;
+    }
+
+    expect(sumOfNumbersInArray([0])).toBe(0);
+});
+
