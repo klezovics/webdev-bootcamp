91f0fdc8a73156f93799ccb1788b8524c50e1614
klezovich <arthur@trevor.io>
2023-05-25 11:02:14 +0200

    Add lesson code

--- src/1_js_practice/5_various_aspects/1_async_javascript/4_async_await_course/lesson_5.js
+++ src/1_js_practice/5_various_aspects/1_async_javascript/4_async_await_course/lesson_5.js
@@ -1,28 +1,25 @@
 // You can resolve multiple promises sequentially or in parallel
 
 const fetch = require("node-fetch");
-const showGitHubUser = async (handle) => {
-    const url = `https://api.github.com/users/${handle}`;
+
+async function fetchFromGithub(endpoint) {
+    const url = `https://api.github.com${endpoint}`;
     const response = await fetch(url);
-
-    //If the promise is rejected, the await keyword throws an exception
-    const body = await response.json();
-
-    // If an issue arrises we can throw an error
-    if (response.status !== 200) {
-        throw Error(body.message);
-    }
-
-    return body;
+    return await response.json();
 }
 
-showGitHubUser('notexistinguser')
-    .then(user => {
-        // Will print 'undefined' because the user does not exist
-        console.log(user.name);
-        console.log(user.location);
-    }).catch(err => {
-        // A thrown exception automatically results in the promise being rejected
-        console.log("Error: ", err.message);
-    }
-)
+async function showUserAndRepos(handle) {
+    const userPromise =  fetchFromGithub(`/users/${handle}`);
+    const reposPromise =  fetchFromGithub(`/users/${handle}/repos`);
+
+    // Now both requests are in flight at the same time
+
+    // The time it takes to complete both requests is the time it takes to complete the slower request
+    const user = await userPromise;
+    const repos = await reposPromise;
+
+    console.log(user.name);
+    console.log(`${repos.length} repos`)
+}
+
+showUserAndRepos("mariusschulz");
