69ee39efbe00f1db8b50b8efdc8b1d3ac0781956
klezovich <arthur@trevor.io>
2023-05-27 14:03:14 +0200

    Add some initial state to redux

--- src/2_react_practice/3_redux/99_idiomatic_redux_course/components/AkToDoList.js
+++ src/2_react_practice/3_redux/99_idiomatic_redux_course/components/AkToDoList.js
@@ -4,8 +4,20 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import todoApp from "../reducers";
 
+const persistedState = {
+  todos: [
+    {
+      id: "0",
+      text: "Welcome back!",
+      completed: false,
+    },
+  ],
+};
+
 const todoAppReducer = todoApp;
-const todoAppStore = createStore(todoAppReducer);
+const todoAppStore = createStore(todoAppReducer, persistedState);
+
+console.log(todoAppStore.getState());
 const AkToDoList = () => (
   <div>
     <Provider store={todoAppStore}>{<DanToDoList />}</Provider>
