cc62fc7adb30a7bfdc3ad1d81a00024a10ec52a9
klezovich <arthur@trevor.io>
2023-05-27 14:13:54 +0200

    Add node-uuid

--- src/2_react_practice/3_redux/99_idiomatic_redux_course/components/AkToDoList.js
+++ src/2_react_practice/3_redux/99_idiomatic_redux_course/components/AkToDoList.js
@@ -3,21 +3,19 @@ import DanToDoList from "./DanToDoList";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import todoApp from "../reducers";
+import { loadState, saveState } from "./localStorage";
 
-const persistedState = {
-  todos: [
-    {
-      id: "0",
-      text: "Welcome back!",
-      completed: false,
-    },
-  ],
-};
+const persistedState = loadState();
 
 const todoAppReducer = todoApp;
 const todoAppStore = createStore(todoAppReducer, persistedState);
 
-console.log(todoAppStore.getState());
+todoAppStore.subscribe(() => {
+  saveState({
+    todos: todoAppStore.getState().todos,
+  });
+});
+
 const AkToDoList = () => (
   <div>
     <Provider store={todoAppStore}>{<DanToDoList />}</Provider>
