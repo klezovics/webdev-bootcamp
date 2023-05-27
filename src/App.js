4fdba99e719debf441de799a0bba0d8c35cea6b5
klezovich <arthur@trevor.io>
2023-05-27 02:47:05 +0200

    Save working version

--- src/App.js
+++ src/App.js
@@ -4,14 +4,36 @@ import ToDoWidgetContainer from "./2_react_practice/3_redux/3_todo_widget/ToDoWi
 import TodoList from "./2_react_practice/3_redux/99_idiomatic_redux_course/components/TodoList";
 import DanToDoList from "./2_react_practice/3_redux/99_idiomatic_redux_course/components/DanToDoList";
 import AkToDoList from "./2_react_practice/3_redux/99_idiomatic_redux_course/components/AkToDoList";
+import { ErrorBoundary } from "react-error-boundary";
+import styled from "styled-components";
 
 function App() {
   const queryClient = new QueryClient();
 
+  function ComponentWithError() {
+    throw new Error("Booom!");
+
+    // ...rest of the component code
+  }
+
+  const Fallback = styled.div`
+    color: red;
+  `;
+
   return (
-    <div className="App">
-      <AkToDoList />
-    </div>
+    <ErrorBoundary
+      fallback={
+        <div>
+          Something went wrong<Fallback>Stay calm!</Fallback>
+        </div>
+      }
+    >
+      {/*<ComponentWithError />*/}
+
+      <div className="App">
+        <AkToDoList />
+      </div>
+    </ErrorBoundary>
   );
 }
 
