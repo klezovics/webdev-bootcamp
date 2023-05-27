a2685ca3270dc3d78890bc06fb4c383d1f12b87a
klezovich <arthur@trevor.io>
2023-05-27 14:45:30 +0200

    Use react router DOM

--- src/2_react_practice/3_redux/99_idiomatic_redux_course/components/AkToDoList.js
+++ src/2_react_practice/3_redux/99_idiomatic_redux_course/components/AkToDoList.js
@@ -3,12 +3,22 @@ import DanToDoList from "./DanToDoList";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import configureStore from "../configureStore";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const todoAppStore = configureStore();
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DanToDoList />,
+  },
+]);
+
 const AkToDoList = () => (
   <div>
-    <Provider store={todoAppStore}>{<DanToDoList />}</Provider>
+    <Provider store={todoAppStore}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
   </div>
 );
 
