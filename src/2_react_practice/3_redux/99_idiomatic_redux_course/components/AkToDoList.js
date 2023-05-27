d65d3a2db9d127bcc40a7193bb42bff8d3a57363
klezovich <arthur@trevor.io>
2023-05-27 14:20:34 +0200

    Finalize adding lodash

--- src/2_react_practice/3_redux/99_idiomatic_redux_course/components/AkToDoList.js
+++ src/2_react_practice/3_redux/99_idiomatic_redux_course/components/AkToDoList.js
@@ -4,17 +4,21 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import todoApp from "../reducers";
 import { loadState, saveState } from "./localStorage";
+import throttle from "lodash/throttle";
 
 const persistedState = loadState();
 
 const todoAppReducer = todoApp;
 const todoAppStore = createStore(todoAppReducer, persistedState);
 
-todoAppStore.subscribe(() => {
-  saveState({
-    todos: todoAppStore.getState().todos,
-  });
-});
+todoAppStore.subscribe(
+  throttle(() => {
+    saveState({
+      todos: todoAppStore.getState().todos,
+    });
+  }),
+  1000
+);
 
 const AkToDoList = () => (
   <div>
