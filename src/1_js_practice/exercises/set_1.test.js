16e92ea7d3c41207bd1c7cafb6a310fc378ddd32
klezovich <arthur@trevor.io>
2023-05-28 20:08:16 +0200

    Add code

--- src/1_js_practice/exercises/set_1.test.js
+++ src/1_js_practice/exercises/set_1.test.js
@@ -1,5 +1,45 @@
 import { info } from "console";
 
+test("test print all numbers 1..n", () => {
+  const printNumbers = (n) => {};
+
+  printNumbers(5);
+});
+
+test("test print all even numbers 1..n", () => {
+  const printNumbers = (n) => {};
+
+  printNumbers(5);
+  // Part 2: Do it also for odd numbers
+});
+
+test("test can reverse an array", () => {
+  const reverseArray = (nums) => {
+    return [];
+  };
+
+  expect(reverseArray([1])).toEqual([1]);
+  expect(reverseArray([1, 2])).toEqual([2, 1]);
+  expect(reverseArray([1, 2, 3, 4])).toEqual([4, 3, 2, 1]);
+
+  expect(reverseArray([1, 1, 5, 5])).toEqual([5, 5, 1, 1]);
+  expect(reverseArray([-1, 1, 3, 2])).toEqual([2, 3, 1, -1]);
+  expect(reverseArray([0, 7, -3])).toEqual([-3, 7, 0]);
+});
+
+test("test print all even numbers 1..n in reverse order", () => {
+  const printNumbers = (n) => {};
+
+  printNumbers(5);
+  // Part 2: Do it also for odd numbers
+});
+
+test("test print all numbers 1..n in reverse order", () => {
+  const printNumbers = (n) => {};
+
+  printNumbers(5);
+});
+
 test("test can find sum of numbers 1,2 .. n", () => {
   const f = (n) => {
     return 0;
@@ -81,6 +121,11 @@ test("test can find subarray sum", () => {
 });
 
 test("test can print 2D array", () => {
+  // On the board draw a 2D array diagram and show basic operations
+  // Before explaining this topic give examples of other arrays
+  // Show an array objects
+  // Array is also an object (typeof) and therefore can be stored as a value in another array
+
   const array2D = [
     [1, 2], // row 0
     [3, 4], // row 1
